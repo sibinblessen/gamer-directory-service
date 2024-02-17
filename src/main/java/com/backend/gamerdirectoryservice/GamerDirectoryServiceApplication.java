eeb4e6e1674a96c7d8a3823ef959b0b839a36c45
Sibin Blessen <sibinblessen@gmail.com>
2024-02-17 10:14:15 +0100

    Initial commit

---
+++ src/main/java/com/backend/gamerdirectoryservice/GamerDirectoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.gamerdirectoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerDirectoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerDirectoryServiceApplication.class, args);
+	}
+
+}
